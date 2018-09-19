c7fb21bb7146908efdad4c2e54398d7f182bc8a9
iO Ninja <ioninja14@gmail.com>
2018-09-19 16:38:33 +1000

    Brackets and line breaks

--- lexer.py
+++ lexer.py
@@ -1,20 +1,27 @@
 import syntax
 
 class Token:
-	def __init__(self, type, value):
+	def __init__(self, type, value = None):
 		self.type = type
 		self.value = value
 
 	def __repr__(self):
-		return self.type + ": " + self.value
+		if self.value:
+			return self.type + ": " + self.value
+		else:
+			return self.type
+
+# Next: brackets as their own type, prevent multiple terminators, prevent terminators after brackets, add keywords
 
 def lex(string):
-	string += " "
+	string = string.replace("\r\n", "\n").replace("\r", "\n") + " "
 	tokens = []
+	brackets = {pair: 0 for pair in syntax.brackets}
 	current = None
 	i = 0
 	while i < len(string):
 		c = string[i]
+
 		if not current:
 			if c in syntax.labelStart:
 				current = Token("label", c)
@@ -23,11 +30,20 @@ def lex(string):
 			elif c in syntax.string:
 				current = Token("string", string[i + 1])
 				i += 1
+			elif c in syntax.terminators and not brackets[max(brackets, key = brackets.get)]:
+				tokens.append(Token("terminator"))
 			else:
 				for symbol in syntax.symbols:
 					sub = string[i : i + len(symbol)]
 					if sub == symbol:
-						tokens.append(Token("symbol", sub))
+						bracket = [(pair, pair.index(symbol)) for pair in syntax.brackets if symbol in pair]
+						if bracket:
+							pair, index = bracket[0]
+							if index == 0:
+								brackets[pair] += 1
+							elif index == 1:
+								brackets[pair] -= 1
+						tokens.append(Token("symbol", symbol))
 						i += len(symbol) - 1
 						break
 		else:
--- syntax.py
+++ syntax.py
@@ -1,23 +1,13 @@
 import string
 
+terminators = ["\n", ";"]
 labelStart = string.ascii_letters + "$" + "_"
 label = labelStart + string.digits
 numberStart = string.digits
 numberDecimal = "."
 number = numberStart + numberDecimal
 string = ["\"", "'"]
-symbols = ["=", "+"]
+brackets = ["()", "[]", "{}"]
+symbols = ["=", "==", "!=", "<", ">", "<=", ">=", "+=", "-=", "*=", "/=", "+", "-", "*", "/", "%", ",", "."]
+for pair in brackets: symbols += list(pair)
 symbols.sort(key = lambda symbol: len(symbol))
-
-# operators = []
-#
-# class Operator:
-# 	def __init__(self, string, number):
-# 		self.string = string
-# 		self.name = name
-# 		self.number = number
-# 		operators.append(self)
-#
-# Operator("=", "assign", 2)
-# Operator("+=", "add", 2)
-# operators.sort(key=lambda operator: len(operator.symbol), reverse=True)
